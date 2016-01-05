6e903e80ac8a9726ab02f1b793c3f6086c0e586c
Eduardo Nogueira <eduardo.nogueira.externo@infoportugal.pt>
2016-01-05 14:20:23 +0000

    Fix page generator.

--- wagtail_autofixtures/models.py
+++ wagtail_autofixtures/models.py
@@ -1,6 +1,7 @@
 from autofixture import register, AutoFixture
 from django.contrib.contenttypes.models import ContentType
 from django.core.management import call_command
+from wagtail.wagtailcore.models import Page
 
 class GenericAutoFixture(AutoFixture):
 
@@ -12,13 +13,27 @@ class GenericAutoFixture(AutoFixture):
     }
 
     @classmethod
-    def generate_page(cls, child_class, fields, number_childs):
-        aux_dict = {'content_type': ContentType.objects.get_for_model(child_class)}
+    def generate_page(cls, child_class, parent_id, number_childs):
+        aux_dict = {
+            'content_type': ContentType.objects.get_for_model(child_class)
+        }
         cls.field_values = dict(cls.field_values, **aux_dict)
-        cls.field_values = dict(cls.field_values, **fields)
         register(child_class, GenericAutoFixture, overwrite=True)
-        call_command('loadtestdata', child_class._meta.__unicode__() + ':' + str(number_childs))
-        call_command('fixtree')
+
+        parent_page = Page.objects.filter(pk=parent_id)[0]
+        parent_path = parent_page.path
+        child_depth = parent_page.depth + 1
+        num_childs = parent_page.numchild
+        for pos in range(0, number_childs):
+            new_page_path = parent_path + str(num_childs + 1).zfill(4)
+            fields = {
+                'path': new_page_path,
+                'depth': child_depth
+            }
+            cls.field_values = dict(cls.field_values, **fields)
+            call_command('loadtestdata', child_class._meta.__unicode__() + ':1')
+            call_command('fixtree')
+            num_childs += 1
 
     class Meta:
         abstract = True
